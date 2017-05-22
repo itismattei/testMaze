a512de18278356540cb3d0dba6b6c0d62c36acbe
itismattei <robotics.itismattei@gmail.com>
2017-05-22 22:20:04 +0200

    Versione iniziale del test sul labirinto

---
+++ path/CreaPercorso.cpp
@@ -0,0 +1,18 @@
+/*
+ * CreaPercorso.cpp
+ *
+ *  Created on: 21/mag/2017
+ *      Author: tps8
+ */
+
+#include "CreaPercorso.h"
+
+CreaPercorso::CreaPercorso() {
+	// TODO Auto-generated constructor stub
+
+}
+
+CreaPercorso::~CreaPercorso() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ path/CreaPercorso.h
@@ -0,0 +1,17 @@
+/*
+ * CreaPercorso.h
+ *
+ *  Created on: 21/mag/2017
+ *      Author: tps8
+ */
+
+#ifndef CREAPERCORSO_H_
+#define CREAPERCORSO_H_
+
+class CreaPercorso {
+public:
+	CreaPercorso();
+	~CreaPercorso();
+};
+
+#endif /* CREAPERCORSO_H_ */
